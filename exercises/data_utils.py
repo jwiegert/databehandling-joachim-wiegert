947c96fd94b89101f9eb6706dd7962021b389bf7
jwiegert <joachimwiegert.astro@gmail.com>
2021-10-21 16:44:44 +0200

    ex01 cont

--- exercises/data_utils.py
+++ exercises/data_utils.py
@@ -7,9 +7,18 @@ def extract_nan_columns(df):
     """
     Extract all columns containing NaNs
     - Input: a dataframe
-    - Output: a dataframe with only NaN-columns
+    - Output: a dataframe with only NaN-columns and a dataframe with number if NaNs
+              per column.
     - Figure: plots these columns
     """
-    df_nans = df[df.columns[df.isna().any() == True]]
-    sns.barplot(data=df_nans)
-    return df_nans
+    # Extract all columns with NaN
+    df_nan = df[df.columns[df.isna().any() == True]]
+    # Save number of Nans
+    Nnans = df_nan.isna().sum()
+    # Put number of Nans in a dataframe (reset index and remove redundant index column)
+    Nnans = pd.DataFrame(
+        {"Column names":Nnans.index, "Number of NaNs":Nnans}).reset_index(drop=True)
+    # Plot
+    sns.barplot(data=Nnans, x="Column names", y="Number of NaNs")
+    # Return data
+    return df_nan, Nnans
