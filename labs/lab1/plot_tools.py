24d02245bbbc6dde3d83a36a1fb8af31940eb8fe
jwiegert <joachimwiegert.astro@gmail.com>
2021-11-03 18:58:11 +0100

    cont lab1 prob3

--- labs/lab1/plot_tools.py
+++ labs/lab1/plot_tools.py
@@ -1,8 +1,26 @@
 import pandas as pd
+
 import plotly
 import plotly_express as px
 from plotly.subplots import make_subplots
 
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+
+# Seaborn line plot function
+def sns_line(data, xparam, yparam, xlabel, ylabel, ax):
+    """
+    Set settings for custom seaborn lineplots and plots
+    Used for loops of subplots
+    """
+    ax.set(ylabel=ylabel, xlabel=xlabel)
+    ax.set_xticks(list(range(0,len(data[xparam]),20)))
+    ax.grid(color="lightgray")
+    sns.lineplot(data=data, x=xparam, y=yparam, ax=ax)
+
+
+# Plotly express function to set settings
 def pxplot_twoline(dataframe1, dataframe2, ylabel1, ylabel2):
     """Plot two lines with two different y-axis in the same figure"""
 
@@ -18,13 +36,11 @@ def pxplot_twoline(dataframe1, dataframe2, ylabel1, ylabel2):
         labels=stdlabels
     ).update_traces(line_color='blue')
 
+    # Move fig2's y-axis to seconday position
     fig2 = px.line(dataframe2, 
         y=ylabel2,
         labels=stdlabels
-    ).update_traces(line_color='red')
-
-    # Move fig2's y-axis to seconday position
-    fig2.update_traces(yaxis="y2")
+    ).update_traces(line_color='red', yaxis="y2")
 
     # Combine figures and set axis labels
     subfig.add_traces(fig1.data + fig2.data)
